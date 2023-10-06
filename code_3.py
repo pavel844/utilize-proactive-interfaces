b3c8f3cb9237acd7dec2f4227404dafabf2149de
Pavel <143156330+pavel844@users.noreply.github.com>
2023-10-06 02:17:46 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+
+    }
+    public static void main(String[] args) {
+}
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+
+        for (int item : data) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        }
+        int[] data = new int[10];
+    const data = generateRandomData();
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+    return data;
+}
+
+}
+
+    const data = [];
+    });
+function main() {
+    }
+        console.log("Random Number: " + item);
