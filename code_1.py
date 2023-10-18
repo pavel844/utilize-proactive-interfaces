df1a4209e9f244be286dcbc0bbea0d49d518c898
Pavel <143156330+pavel844@users.noreply.github.com>
2023-10-18 16:57:39 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,40 +1,14 @@
+def generate_random_data():
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
 
 import random
         print(f"Random Number: {item}")
-    main()
-    data = generate_random_data()
     return data
+    main()
+    for item in data:
 
 if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-
-def generate_random_data():
-
-def main():    data = generate_random_data()
-
-    for item in data:
-
-import random
-    return data
-def generate_random_data():
-    main()
 def main():
-
-        print(f"Random Number: {item}")
-
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":if __name__ == "__main__":
-
-
-
     data = generate_random_data()
-        print(f"Random Number: {item}")
-    for item in data:
-    return data
-def main():
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def generate_random_data():
-    main()
